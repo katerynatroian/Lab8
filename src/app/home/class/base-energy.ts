79510d09084d558d31694f345a2e0ab5194531c5
Kateryna <troyankate6@gmail.com>
2025-05-15 13:07:48 +0300

    second commit 2.0

--- src/app/home/class/base-energy.ts
+++ src/app/home/class/base-energy.ts
@@ -8,16 +8,13 @@ export abstract class BaseEnergy implements IEnergy {
 
     charge(): void {
         this.power += 10;
-        console.log(`${this.constructor.name} заряджено. Потужність: ${this.power}`);
     }
 
     release(): void {
-        console.log(`${this.constructor.name} випущено з силою: ${this.power}`);
         this.power = 0;
     }
 
     takeDamage(damage: number): void {
         this.power = Math.max(0, this.power - damage);
-        console.log(`${this.constructor.name} ослаблено. Потужність: ${this.power}`);
     }
 }
